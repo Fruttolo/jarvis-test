db3321c0e1e4c10aceee2c08675f80b56bd50774
fruttolo <s.contino97@gmail.com>
2025-08-08 11:53:21 +0200

    tolto print da call_chatgpt

--- chatGPTcall.py
+++ chatGPTcall.py
@@ -6,7 +6,6 @@ load_dotenv()
 
 def call_chatgpt(prompt: str, model: str = "gpt-3.5-turbo") -> str:
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    print(os.getenv("OPENAI_API_KEY"))
     response = openai.chat.completions.create(
         model=model,
         messages=[{"role": "user", "content": prompt}]
